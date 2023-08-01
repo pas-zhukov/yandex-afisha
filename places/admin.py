35a011e5cf930b4455e70c8c654797fc583ac954
Pavel <pas-zhukov@yandex.ru>
2023-08-01 20:44:56 +0300

    add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    readonly_fields = ['coords']
+
+    def coords(self, obj):
+        return f'{obj.coordinates}'
--- places/models.py
+++ places/models.py
@@ -1,4 +1,29 @@
+from dataclasses import dataclass
+from decimal import Decimal
+
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+@dataclass
+class Coordinates:
+    latitude: Decimal
+    longitude: Decimal
+
+    def __str__(self):
+        return f'{round(self.latitude, 5)} {round(self.longitude, 5)}'
+
+
+class Place(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.lat and self.long:
+            self.coordinates = Coordinates(latitude=self.lat, longitude=self.long)
+
+    title = models.CharField(max_length=200, null=True)
+    description_short = models.TextField(null=True)
+    description_long = models.TextField(null=True)
+    lat = models.DecimalField(max_digits=20, decimal_places=16)
+    long = models.DecimalField(max_digits=20, decimal_places=16)
+
+    def __str__(self):
+        return f'{self.id}. {self.title.strip()}'
