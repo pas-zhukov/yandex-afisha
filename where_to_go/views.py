2578c6957c5c9547e5cf897aadcb73f5b5a6df80
Pavel <pas-zhukov@yandex.ru>
2023-08-02 17:41:24 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
-
+import json
 
 def index(request):
     template = loader.get_template('index.html')
-    context = {}
+    with open('static/places/geo_json.json', 'rb') as file:
+        places = json.load(file)
+        context = {
+            'geo_json': places
+
+                   }
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
