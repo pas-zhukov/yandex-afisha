caf398082cd5ba662b0179a01a989e93b7ceb650
Pavel <pas-zhukov@yandex.ru>
2023-08-03 22:31:48 +0300

    add static for deploy

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from places import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     path('places/<int:place_id>/', views.place, name='place-json'),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
