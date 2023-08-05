faec906dfc6d56dc874fb26d9ff843c7c8c641db
Pavel <pas-zhukov@yandex.ru>
2023-08-05 11:49:52 +0300

    remove coords field

--- places/models.py
+++ places/models.py
@@ -4,21 +4,7 @@ from tinymce.models import HTMLField
 from django.db import models
 
 
-@dataclass
-class Coordinates:
-    latitude: Decimal
-    longitude: Decimal
-
-    def __str__(self):
-        return f'{round(self.latitude, 5)} {round(self.longitude, 5)}'
-
-
 class Place(models.Model):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.lat and self.long:
-            self.coordinates = Coordinates(latitude=self.lat, longitude=self.long)
-
     title = models.CharField(max_length=200, verbose_name='Название', null=False)
     description_short = models.TextField(verbose_name='Краткое описание', null=False)
     description_long = HTMLField(verbose_name='Полное описание', null=False)
